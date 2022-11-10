b6b3a19644699ad20d19d39fadc93775aacf75b2
saad ahmed <saadahmed1742@gmail.com>
2022-11-10 17:13:41 +0500

    signup and login page UI completed

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBF9AwSVNvNzOsm5X9uqz6MbtzwJbzvqCE",
+    authDomain: "hackaton-74144.firebaseapp.com",
+    projectId: "hackaton-74144",
+    storageBucket: "hackaton-74144.appspot.com",
+    messagingSenderId: "534241085328",
+    appId: "1:534241085328:web:d110cb7d33827c12131515"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
